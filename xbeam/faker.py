14d0fc48a5bf9791d91db05e3d0b497ce68def4f
Kevin Rose <kevin+gh@maypark.com>
2021-11-23 21:20:39 -0600

    refactor faker a little

--- xbeam/faker.py
+++ xbeam/faker.py
@@ -1,18 +1,31 @@
+from io import BytesIO
+
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+from matplotlib import patches
+from PIL import Image
+import numpy as np
 
 from puzzle import Puzzle
 
 
-def plot_grid(puz: Puzzle) -> Axes:
-    rows, cols = puz.boxes.shape
-    ax = plt.subplots(1)[1]
-    ax.imshow(
-        puz.boxes[::-1, :],
-        cmap="gray",
-        origin="lower",
-        extent=(0, cols, 0, rows),
+def _draw_square(ax: Axes, i: int, j: int, color=tuple):
+    ax.add_patch(
+        plt.Polygon(
+            [(j, i), (j, i - 1), (j + 1, i - 1), (j + 1, i)],
+            edgecolor=None,
+            facecolor=color,
+        )
     )
+
+
+def plot_grid(puz: Puzzle, ax: Axes = None) -> Axes:
+    rows, cols = puz.boxes.shape
+    if ax is None:
+        ax = plt.subplots(1)[1]
+    ax.set_xlim([0, cols])
+    ax.set_ylim([0, rows])
+    ax.set_aspect(1)
 
     ax.set_xticks(range(cols))
     ax.set_yticks(range(rows))
@@ -23,24 +36,19 @@ def plot_grid(puz: Puzzle) -> Axes:
 
     for i in range(rows):
         for j in range(cols):
+            if puz.boxes[i, j] == 0:
+                _draw_square(ax, rows - i, j, (0, 0, 0))
             if puz.shades is not None and puz.shades[i, j]:
-                ax.add_patch(
-                    plt.Polygon(
-                        [
-                            (j, rows - i),
-                            (j, rows - i - 1),
-                            (j + 1, rows - i - 1),
-                            (j + 1, rows - i),
-                        ],
-                        edgecolor=None,
-                        facecolor=(0, 0, 0, 0.3),
-                    )
-                )
+                _draw_square(ax, rows - i, j, (0, 0, 0, 0.3))
             if puz.circles is not None and puz.circles[i, j]:
                 ax.add_patch(
-                    plt.Circle(
+                    patches.Arc(
                         (j + 0.5, rows - i - 0.5),
-                        0.49,
+                        0.95,
+                        0.95,
+                        angle=160,
+                        theta1=0,
+                        theta2=300 if puz.numbers[i, j] > 0 else 360,
                         edgecolor=(0, 0, 0),
                         facecolor="none",
                         linewidth=0.75,
@@ -55,10 +63,26 @@ def plot_grid(puz: Puzzle) -> Axes:
                     fontweight=500,
                     horizontalalignment="left",
                     verticalalignment="top",
-                    rasterized=True,
                 )
 
     return ax
+
+
+def grid2pixels(puz: Puzzle) -> np.ndarray:
+    f, ax = plt.subplots(1)
+    plot_grid(puz, ax)
+    b = BytesIO()
+    plt.savefig(
+        b,
+        format="png",
+        transparent=True,
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0.01,
+    )
+    plt.close(f)
+    b.seek(0)
+    return np.array(Image.open(b))
 
 
 if __name__ == "__main__":
@@ -68,8 +92,20 @@ if __name__ == "__main__":
 
     with open(Path(__file__).parents[1] / "data/2021-09-12.json") as f:
         plot_grid(parse(json.load(f)))
+        plt.savefig(
+            "/Users/kevin/Desktop/Figure_1.png",
+            transparent=True,
+            dpi=200,
+            bbox_inches="tight",
+            pad_inches=0.01,
+        )
 
-    # with open(Path(__file__).parents[1] / "data/2021-09-14.json") as f:
-    #     plot_grid(parse(json.load(f)))
-
-    plt.show()
+    with open(Path(__file__).parents[1] / "data/2021-09-14.json") as f:
+        plot_grid(parse(json.load(f)))
+        plt.savefig(
+            "/Users/kevin/Desktop/Figure_2.png",
+            transparent=True,
+            dpi=200,
+            bbox_inches="tight",
+            pad_inches=0.01,
+        )
